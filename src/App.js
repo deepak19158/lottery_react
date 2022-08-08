aee2677aa81291f1066d9ac052e369015d4ca345
deepak <dg91918@gmail.com>
2022-08-08 17:44:16 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    address: "",
+    value: "",
+    status: "-",
+  };
+
+  componentDidMount = async () => {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayer().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    const address = lottery.options.address;
+    this.setState({ manager, balance, address, players });
+  };
+
+  pickWinner = async (event) => {
+    event.preventDefault();
+
+    const account = await web3.eth.getAccounts();
+    await lottery.methods.pickWinner().send({
+      from: account[0],
+    });
+  };
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const account = await web3.eth.getAccounts();
+
+    this.setState({ status: "wait we are processing" });
+    await lottery.methods.enter().send({
+      from: account[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+
+    this.setState({ status: "processed" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Lottery Contract</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The manager of this contract is {this.state.manager} and the address
+          is {this.state.address} and {this.state.players.length} are competing
+          to win {web3.utils.fromWei(this.state.balance, "ether")} ether. All
+          the players are {this.state.players}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4> Wanna try your luck</h4>
+          <div>
+            <label>Enter the amount </label>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>submit</button>
+        </form>
+
+        <hr />
+
+        <h4>{this.state.status}</h4>
+        <hr />
+
+        <form onSubmit={this.pickWinner}>
+          <button> Pick a winner</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,58 @@
+import web3 from "./web3";
+
+const address = "0x3464cA12D05387E65437082b192700a11d5805b3";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayer",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
